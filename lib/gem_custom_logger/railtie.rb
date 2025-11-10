f8f36185485a3e8376072a0b3d9c81a6eac4ec98
Karina <karinasokolova252@gmail.com>
2025-11-10 22:31:11 +0200

    Add tasks in initializer

--- lib/gem_custom_logger/railtie.rb
+++ lib/gem_custom_logger/railtie.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require 'gem_custom_logger'
+require 'rails/railtie'
 
 module GemCustomLogger
   class Railtie < Rails::Railtie
+    rake_tasks do
+      load File.expand_path("../tasks/custom_logger.rake", __dir__)
+    end
+    
     initializer "gem_custom_logger.configure_rails_initialization" do
       GemCustomLogger::CustomLogger.print
     end
