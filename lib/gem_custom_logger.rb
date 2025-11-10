83fe65d862d4b39cc42601a53eff0c17dde91a0f
Karina <karinasokolova252@gmail.com>
2025-11-10 22:08:21 +0200

    Fix requires

--- lib/gem_custom_logger.rb
+++ lib/gem_custom_logger.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "gem_custom_logger/version"
-require "gem_custom_logger/custom_logger"
 require "gem_custom_logger/railtie" if defined?(Rails)
 
 module GemCustomLogger
