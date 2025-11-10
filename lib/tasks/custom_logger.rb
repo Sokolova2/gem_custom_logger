4071bc82a2bc873be339c222970b7a4fda5d3da5
Karina <karinasokolova252@gmail.com>
2025-11-10 22:24:45 +0200

    Fix output

--- lib/tasks/custom_logger.rb
+++ lib/tasks/custom_logger.rb
@@ -3,6 +3,6 @@
 namespace :custom_logger do
   desc "This task for custom rails command"
   task :custom_logger, [:message] => environment do |t, args|
-    p "My custom logger: #{args.message || 'Welcome in GemCustomLogger'}"
+    puts "My custom logger: #{args.message || 'Welcome in GemCustomLogger'}"
   end
 end
